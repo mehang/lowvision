c4741739463936a2ae2f48d36781c163896ebf93
Sanjel <arun_sanjel1@baylor.edu>
2019-07-12 13:15:22 -0500

    Added New Style and Jquery

--- phone.js
+++ phone.js
@@ -1,12 +1,40 @@
-<html>
-	<head>
-		<title> Assignment 1 </title>
-	</head>
-	<link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css" integrity="sha384-nn4HPE8lTHyVtfCBi5yW9d20FjT8BJwUXyWZT9InLYax14RDjBj46LmSztkmNP9w" crossorigin="anonymous">
-	<body>
-	<div class = "pure-g" id="main">
-		<div class = "pure-u-1" id="displayPanel"> 
-		</div>
-	</div>
-	</body>
-</html>
+$(document).ready(function(){
+	$("#contactList").hide();
+	$("#addContact").hide();
+		
+});
+
+$("#btnAdd").click(function(){
+	$("#dialerTab").hide();
+	$("#contactList").hide();
+	$("#addContact").show();
+});
+
+$("#btnDailer").click(function(){
+	$("#dialerTab").show();
+	$("#contactList").hide();
+	$("#addContact").hide();
+});
+$("#btnContact").click(function(){
+	$("#dialerTab").hide();
+	$("#contactList").show();
+	$("#addContact").hide();
+});
+
+$(".dialButton").click(function(){
+	var dialDisplay = $("#display");
+	var number = $(this).text();
+	dialDisplay.val(dialDisplay.val() + number);
+});
+
+$("#Clear").click(function(){
+	$("#display").val("");
+});
+
+$("#Clear2").click(function(){
+	$("#dialerTab").hide();
+	$("#contactList").show();
+	$("#addContact").hide()
+	var nameDisplay = $("#name");
+	name.val("");
+});
